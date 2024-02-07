77b958ac1e768093822bd0c00eba6aec58ec1374
MHusain22 <husain.vijapura@gmail.com>
2024-02-07 16:12:39 +0530

    changes

--- client/src/Data/data.js
+++ client/src/Data/data.js
@@ -8,7 +8,7 @@ export const blogList = [
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     authorName: 'John Doe',
     authorAvatar: '/assets/images/author.jpg',
-    createdAt: 'June 03, 2021',
+    createdAt: 'June 23, 2021',
     cover: '/assets/images/designer-1.jpg',
   },
   {
