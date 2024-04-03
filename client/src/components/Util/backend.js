a56b0db4ac1ff1d4e13151ba36a525766d8a8863
MHusain22 <husain.vijapura@gmail.com>
2024-04-03 11:39:27 +0530

    url change

--- client/src/components/Util/backend.js
+++ client/src/components/Util/backend.js
@@ -1,3 +1,3 @@
-// const API_URL = "https://blog-website-331b.onrender.com"
-const API_URL = "http://localhost:5000";
+const API_URL = "https://blog-website-331b.onrender.com"
+// const API_URL = "http://localhost:5000";
 export default API_URL;
